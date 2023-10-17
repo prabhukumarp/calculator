5f024440d21220bc802c6df2a4eab2e92961dfe7
Prabhu Kumar <prabhukumarp@gmail.com>
2023-10-17 23:03:53 +0530

    Updated changes to calculator app

--- calc.js
+++ calc.js
@@ -1,59 +1,82 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const inputElement = document.getElementById('input');
-    const outputElement = document.getElementById('output');
+let inputRow = '';
+let outputRow = '';
+let leftParenthesesCount = 0;
 
-    let currentInput = '';
-    
-    function updateDisplay() {
-        inputElement.innerText = currentInput;
-        outputElement.innerText = '';
-    }
+function clearDisplay() {
+    inputRow = '';
+    outputRow = '';
+    leftParenthesesCount = 0;
+    updateDisplay();
+}
 
-    function clearAll() {
-        currentInput = '';
-        updateDisplay();
-    }
-
-    function backspace() {
-        currentInput = currentInput.slice(0, -1);
-        updateDisplay();
-    }
-
-    function calculate() {
-        try {
-            const result = eval(currentInput);
-            outputElement.innerText = result;
-        } catch (error) {
-            outputElement.innerText = 'Error';
+function appendCharacter(character) {
+    inputRow += character;
+    if (character === '(') {
+        leftParenthesesCount++;
+    } else if (character === ')') {
+        if (leftParenthesesCount > 0) {
+            // inputRow += ')';
+            leftParenthesesCount--;
         }
     }
+    updateDisplay();
+}
 
-    function handleButtonClick(value) {
-        switch (value) {
-            case 'AC':
-                clearAll();
-                break;
-            case 'backspace':
-                backspace();
-                break;
-            case '=':
-                calculate();
-                break;
-            default:
-                currentInput += value;
-                updateDisplay();
-        }
+function toggleParentheses() {
+    if (leftParenthesesCount === 0) {
+        appendCharacter('(');
+    } else {
+        appendCharacter(')');
     }
+}
 
-    const buttons = ['AC', '()', '%', '/', '7', '8', '9', '*', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', 'backspace', '='];
+function backspace() {
+    if (inputRow.charAt(inputRow.length - 1) === '(') {
+        leftParenthesesCount--;
+    } else if (inputRow.charAt(inputRow.length - 1) === ')') {
+        leftParenthesesCount++;
+    }
+    inputRow = inputRow.slice(0, -1);
+    updateDisplay();
+}
 
-    const buttonsContainer = document.querySelector('.buttons');
+function calculateResult() {
+    try {
+        outputRow = eval(inputRow).toString();
+        updateDisplay();
+        inputRow = '0';
+        leftParenthesesCount = 0;
+    } catch (error) {
+        outputRow = 'Error';
+        updateDisplay();
+    }
+}
 
-    buttons.forEach(value => {
-        const button = document.createElement('button');
-        button.innerText = value;
-        button.classList.add('btn', 'btn-outline-dark', 'm-1', 'col-md-3', 'col-3');
-        button.addEventListener('click', () => handleButtonClick(value));
-        buttonsContainer.appendChild(button);
-    });
-});
+function updateDisplay() {
+    document.getElementById('inputRow').innerText = inputRow;
+    document.getElementById('outputRow').innerText = outputRow;
+}
+
+// Theme Toggle Function
+function toggleTheme() {
+    const body = document.body;
+    const calculator = document.querySelector('.calculator');
+    const moonIcon = document.getElementById('moonIcon');
+    const sunIcon = document.getElementById('sunIcon');
+
+    body.classList.toggle('dark-mode');
+    body.classList.toggle('light-mode');
+    calculator.classList.toggle('dark-mode');
+    calculator.classList.toggle('light-mode');
+
+    const isDarkMode = body.classList.contains('dark-mode');
+
+    // Toggle visibility of moon and sun icons
+    if (isDarkMode) {
+        moonIcon.style.display = 'none';
+        sunIcon.style.display = 'inline-block';
+    } else {
+        moonIcon.style.display = 'inline-block';
+        sunIcon.style.display = 'none';
+    }
+}
